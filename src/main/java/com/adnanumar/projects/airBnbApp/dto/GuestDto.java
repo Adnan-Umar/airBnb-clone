012db98c185a6d3616ee8d6bd9922a03b8d7fd89
Md Adnan Umar <adnanmd0786@gmail.com>
2026-02-01 00:42:44 +0530

    fix get my booking DTO

--- src/main/java/com/adnanumar/projects/airBnbApp/dto/GuestDto.java
+++ src/main/java/com/adnanumar/projects/airBnbApp/dto/GuestDto.java
@@ -1,6 +1,5 @@
 package com.adnanumar.projects.airBnbApp.dto;
 
-import com.adnanumar.projects.airBnbApp.entity.User;
 import com.adnanumar.projects.airBnbApp.entity.enums.Gender;
 import lombok.AccessLevel;
 import lombok.Data;
@@ -11,8 +10,6 @@ import lombok.experimental.FieldDefaults;
 public class GuestDto {
 
     Long id;
-
-    User user;
 
     String name;
 
